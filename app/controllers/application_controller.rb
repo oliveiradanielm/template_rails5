2249129818ddf2fd5b6a85874c71813698c4426c
Daniel Oliveira <oliveira.danielm@gmail.com>
2018-08-10 16:50:21 -0300

    Internationalization added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    puts 'DEBUG' + '**'*20
+    puts params[:locale].inspect
+    puts '**'*20
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
